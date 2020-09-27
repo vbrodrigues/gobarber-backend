de3396f7b250bad69ca1ec0e6c9733c5456e294b
unknown <vitorborbarodrigues@gmail.com>
2020-09-27 14:15:31 -0300

    eslint; applying DDD and Liskov Substitution Principle

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -2,10 +2,10 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
-import AppError from '../../../shared/errors/AppError';
-import User from '../entities/User';
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
 
-import authConfig from '../../../config/auth';
+import authConfig from '@config/auth';
 
 interface Request {
     email: string;
